2950b3da0fe7bd479eaaa23cfbd70eb671765ad9
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-11 16:20:37 +1100

    attempt to get user secrets working locally but not for some reason

--- order/Program.cs
+++ order/Program.cs
@@ -1,9 +1,19 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureAppConfiguration((hostContext, config) =>
+    {
+        if (hostContext.HostingEnvironment.IsDevelopment())
+        {
+            config.AddJsonFile("local.settings.json");
+            config.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+        }
+    })
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
