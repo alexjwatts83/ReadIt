d2502e7e6bbb1a4aba3d8a7f57c769fcb362b45b
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-11 15:48:04 +1100

    enable comos

--- order/ProcessOrderCosmos.cs
+++ order/ProcessOrderCosmos.cs
@@ -1,11 +1,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
-using System.Collections.Generic;
 using Microsoft.Azure.Functions.Worker;
 
 namespace AzureCourse.Function
-{    
+{
     public class CosmosOrderFunction
     {
         private readonly ILogger<CosmosOrderFunction> _logger;
@@ -16,18 +15,17 @@ namespace AzureCourse.Function
         }
 
         [Function("ProcessOrderCosmos")]
-        //[CosmosDBOutput(databaseName: "readit-orders", containerName: "orders", Connection = "CosmosDBConnection",CreateIfNotExists =true)]            
+        [CosmosDBOutput(databaseName: "readit-orders", containerName: "orders", Connection = "CosmosDBConnection", CreateIfNotExists = true)]
         public object Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req)            
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req)
         {
             string requestBody = new StreamReader(req.Body).ReadToEndAsync().Result;
 
             _logger.LogInformation($"Order JSON: {requestBody}");
 
-            return "OK";
-            //var order=JsonSerializer.Deserialize<Order>(requestBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true })??new Order();
-            //order.id=Guid.NewGuid().ToString();
-            //return order;
+            var order = JsonSerializer.Deserialize<Order>(requestBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? new Order();
+            order.id = Guid.NewGuid().ToString();
+            return order;
         }
     }
 }
